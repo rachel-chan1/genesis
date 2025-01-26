850d380164d34fb9e8260e3576a5981dec7bacdc
nicholegodfrey <nicholegodfrey10@gmail.com>
2025-01-25 23:18:45 -0800

    changes chlothing package

--- app/main 2.js
+++ app/main 2.js
@@ -2,4 +2,3 @@ import { registerRootComponent } from 'expo';
 import Navigation from './Navigation'; // Import Navigation setup
 
 registerRootComponent(Navigation); // Register Navigation as the root component
-export { Navigation };
