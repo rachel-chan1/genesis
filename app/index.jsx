d7d8472d93c720bebffc778c39745032fe0f695e
Rachel Chan <rchan2023@gmail.com>
2025-01-25 17:17:13 -0800

    make feed

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,55 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
 import frog from "@/assets/images/download (41).jpeg"
 import Cover from "@/components/home/cover"
+import Feed from "@/components/home/feed"
 
-const app = () => {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Cover />
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Cover />
+        <Feed />
+        <View style={{ height: 20 }} />
+      </ScrollView>
+      <View style={styles.newButtonOut}>
+        <View style={styles.newButtonIn}>
+          <Text style={{ color: '#36522A', fontSize: 75, marginTop: -18 }}>+</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
-export default app
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  newButtonOut: {
+    width: 72,
+    height: 72,
+    backgroundColor: '#36522A',
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newButtonIn: {
+    width: 62,
+    height: 62,
+    backgroundColor: '#E6D2DB',
+    position: 'absolute',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
